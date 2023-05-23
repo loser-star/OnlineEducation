ca69258ba36bc4e7f29c1824e51bbcedeb5f7bc9
没了酒的李白 <8449554+txp2039442945@user.noreply.gitee.com>
2023-05-23 23:39:18 +0800

    前端项目搭建

---
+++ water-drop/craco.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CracoLessPlugin = require('craco-less');
+const resolve = dir => path.resolve(__dirname, dir);
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options:{
+        lessLoaderOptions: {
+          lessOptions:{
+            modifyVars:{},
+            javascriptEnabled:true,
+          }
+        }
+      }
+    },
+  ],
+  webpack: {
+    alias: {
+      '@': resolve("src"),
+      'components': resolve("src/components"),
+      // '@mui/styled-engine': '@mui/styled-engine-sc'
+    }
+  }
+}
